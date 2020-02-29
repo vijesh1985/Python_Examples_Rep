cfbae29d6c0078decf06c0f48fe2a4a94bbac7b4
vijesh1985 <vijesh_viju@yahoo.co.in>
2020-02-29 11:25:28 +0530

    02/Oct/2019 11:09

---
+++ my_first_gui.py
@@ -0,0 +1,7 @@
+from tkinter import * 		# Importing the Tkinter package
+window = Tk() 				# Tk() is creating the window that you see and storing it inside the 'window' variable
+window.geometry('300x400')  # This is the dimension of the window. Height and width (optional)
+window.title("Vijesh's Window")		# To change the title of the window
+lbl = Label(window, text="Hello")
+lbl.grid(column=0, row=0)
+window.mainloop()			# Make sure the window stays and doesn't disappear.
