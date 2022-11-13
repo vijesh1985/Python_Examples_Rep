fa091542958917d210c8a5ac6ba342ac677805d5
vijesh1985 <vijesh_viju@yahoo.co.in>
2022-11-13 15:29:49 +0530

    km to mile converter

---
+++ km_to_mile_conversion.py
@@ -0,0 +1,6 @@
+kms = float(input("Enter the value in kilometers: "))
+conv_fact = 0.621371
+miles = kms * conv_fact
+print('%0.2f Kilometers is equal to %0.2f miles' %(kms,miles))
+print('Miles = %0.2f miles' %miles)
+print("Miles = ", miles)
